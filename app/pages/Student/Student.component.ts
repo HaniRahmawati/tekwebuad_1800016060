25c6c0e69e473ea6ca3431e3d1db5e7c06f54961
unknown <hani1800016060@webmail.uad.ac.id>
2020-04-01 21:07:39 +0700

    second commit

---
+++ app/pages/Student/Student.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './Student.component.html',
+  styleUrls: ['./Student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
